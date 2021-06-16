4b6c2d60be770cec8d631fedec1ac184e0e5b613
zhixing-YU <co10ans02@gmail.com>
2021-06-16 14:53:37 +0800

    add

--- WebApplication5/WebApplication5/registered.aspx.cs
+++ WebApplication5/WebApplication5/registered.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -23,6 +24,7 @@ namespace WebApplication5
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            /*
             if (TextBox1.Text == "root" && TextBox2.Text == "123456")
             {
                 HttpCookie cookie = new HttpCookie("user");
@@ -33,6 +35,44 @@ namespace WebApplication5
             else {
                 Label2.Text = "帳號或密碼錯誤!!";
             }
+            */
+            /*
+            string s_data = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Computer_productionConnectionString"].ConnectionString;
+
+            SqlConnection Connection = new SqlConnection(s_data);
+
+            string sqlTset = "select * from accounts_Mbmbers";
+
+            SqlCommand Command = new SqlCommand(sqlTset,Connection);
+
+            Connection.Open();
+
+            SqlDataReader Reader = Command.ExecuteReader();
+
+            if (Reader.HasRows)
+            {
+                if (Reader.Read())
+                {
+                    Label2.Text = Reader["密碼"].ToString();
+                }
+            }
+            Connection.Close();
+            */
+            string s_data = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Computer_productionConnectionString"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(s_data);
+
+            SqlCommand command = new SqlCommand("select * from accounts_Mbmbers", connection);
+
+            connection.Open();
+
+            SqlDataReader Reader = command.ExecuteReader();
+
+            if (Reader.Read())
+            {
+                Label2.Text = Reader[5].ToString();
+            }
+            connection.Close();
         }
 
         protected void TextBox1_TextChanged(object sender, EventArgs e)
